2f5470df31d8513485fdbbe83624300111c6acc6
ldynia <ludd@cbs.dtu.dk>
2019-02-06 17:14:53 +0100

    adding spike method

--- xml-rpc/client.py
+++ xml-rpc/client.py
@@ -1,11 +1,13 @@
 #!/usr/local/bin/python
 
 import signal
+import signal
 import xmlrpc.client
 
-with xmlrpc.client.ServerProxy("http://rpc-server/") as proxy:
+with xmlrpc.client.ServerProxy("http://rpc-server:80/") as proxy:
     print("3 is even: %s" % str(proxy.is_even(3)))
     print("100 is even: %s" % str(proxy.is_even(100)))
+    #print("CPU spike run for: %s seconds" % str(proxy.spike_cpu(3)))
 
 print("Press CTRL+C to end ..")
 signal.pause()
--- xml-rpc/server.py
+++ xml-rpc/server.py
@@ -1,5 +1,8 @@
 #!/usr/local/bin/python
 
+import time
+import subprocess
+
 from xmlrpc.server import SimpleXMLRPCServer
 
 
@@ -7,10 +10,17 @@ def is_even(n):
     return n % 2 == 0
 
 
+def spike_cpu(duration_seconds):
+    subprocess.run(["dd", "if=/dev/zero", "of=/dev/null"])
+    time.sleep(duration_seconds)
+    return duration_seconds
+
+
 server = SimpleXMLRPCServer(("0.0.0.0", 80))
 print("Listening on port 80...")
 
 server.register_function(is_even, "is_even")
+server.register_function(spike_cpu, "spike_cpu")
 
 print("Press CTRL+C to end ..")
 server.serve_forever()
